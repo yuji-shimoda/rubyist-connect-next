8b9e2f61b30a8e351121781b25f77691d14e6610
Yuji Shimoda <yuji.shimoda@gmail.com>
2022-09-06 14:43:00 +0900

    feature: add header & meta tag for ogp.

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { useUser } from '@supabase/auth-helpers-react';
 import { supabaseClient } from '@supabase/auth-helpers-nextjs';
 import { Button, Text, Container, Spacer } from '@nextui-org/react';
@@ -18,29 +19,40 @@ export default function LoginPage() {
   if (error) return <p>{error.message}</p>;
   if (!user)
     return (
-      <Container
-        as="main"
-        display="flex"
-        direction="column"
-        justify="center"
-        alignItems="center"
-        style={{ height: '100vh' }}>
-        <Spacer />
-        <Text
-          h1
-          size={60}
-          css={{
-            textGradient: '45deg, $purple600 -20%, $pink600 100%',
-          }}
-          weight="bold">
-          Rubyist Connect
-        </Text>
-        <Text>Search for Rubyists close to you and connect with Rubyists.</Text>
-        <Spacer />
-        <Button color="error" ghost onPress={login}>
-          Sign in with GitHub
-        </Button>
-      </Container>
+      <>
+        <Head>
+          <title>Rubyist Connect</title>
+          <meta property="og:title" content="Rubyist Connect" />
+          <meta
+            property="og:description"
+            content="Search for Rubyists close to you and connect with Rubyists."
+          />
+          <meta property="og:image" content="/ruby.png" />
+        </Head>
+        <Container
+          as="main"
+          display="flex"
+          direction="column"
+          justify="center"
+          alignItems="center"
+          style={{ height: '100vh' }}>
+          <Spacer />
+          <Text
+            h1
+            size={60}
+            css={{
+              textGradient: '45deg, $purple600 -20%, $pink600 100%',
+            }}
+            weight="bold">
+            Rubyist Connect
+          </Text>
+          <Text>Search for Rubyists close to you and connect with Rubyists.</Text>
+          <Spacer />
+          <Button color="error" ghost onPress={login}>
+            Sign in with GitHub
+          </Button>
+        </Container>
+      </>
     );
   router.push('/nnect');
 }
