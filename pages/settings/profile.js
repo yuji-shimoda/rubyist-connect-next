27e3522c75745618372ff32e450cc9917466cc0c
Yuji Shimoda <yuji.shimoda@gmail.com>
2022-09-10 16:53:28 +0900

    feature: add name & website edit function.

--- pages/settings/profile.js
+++ pages/settings/profile.js
@@ -8,6 +8,7 @@ import {
   Textarea,
   Button,
   Spacer,
+  Input,
 } from "@nextui-org/react";
 import { useRouter } from "next/router";
 import { useForm, Controller } from "react-hook-form";
@@ -20,16 +21,26 @@ export default function UserPage() {
   const { user } = useUser();
   const { control, setValue, getValues } = useForm({
     defaultValues: {
+      name: "",
       introduction: "",
+      website: "",
     },
   });
 
   async function updateProfile() {
     try {
-      const introduction = getValues("introduction");
+      const form = getValues();
       const { data, error } = await supabaseClient
         .from("profiles")
-        .update({ introduction: introduction, updated_at: new Date() })
+        .update({
+          name: form.name,
+          introduction: form.introduction,
+          updated_at: new Date(),
+          nickname: user?.user_metadata?.user_name,
+          github_id: user?.user_metadata?.sub,
+          avatar_url: user?.user_metadata?.avatar_url,
+          website: form.website,
+        })
         .match({ id: user.id });
       if (error) {
         throw error;
@@ -40,14 +51,16 @@ export default function UserPage() {
       console.error(err.message);
     }
   }
-
   useEffect(() => {
     async function loadData() {
       try {
         const res = await fetch(`/api/users/${user?.user_metadata?.user_name}`);
         if (res.ok) {
           const data = await res.json();
-          setValue("introduction", data.pop().introduction);
+          const me = data.pop();
+          setValue("name", me.name);
+          setValue("introduction", me.introduction);
+          setValue("website", me.website);
         }
       } catch (error) {
         console.error(error);
@@ -60,11 +73,32 @@ export default function UserPage() {
     <>
       <AppBar user={user} />
       <Container>
-        <Grid.Container gap={2} justify="center" css={{ mt: 20 }}>
+        <Grid.Container gap={2} justify="left" css={{ mt: 20 }}>
           <Grid sm md lg xl />
           <>
             <Text
-              h2
+              h3
+              transform="full-width"
+              size={60}
+              css={{
+                textGradient: "45deg, $blue600 -20%, $pink600 50%",
+              }}
+              weight="bold"
+            >
+              Name
+            </Text>
+            <Spacer />
+            <Controller
+              name="name"
+              control={control}
+              fullWidth
+              render={({ field }) => (
+                <Input aria-label="name" fullWidth type="text" {...field} />
+              )}
+            />
+            <Spacer />
+            <Text
+              h3
               transform="full-width"
               size={60}
               css={{
@@ -88,8 +122,30 @@ export default function UserPage() {
                   size="xl"
                   color="secondary"
                   placeholder="Introduction"
+                  aria-label="introduction"
                   {...field}
                 />
+              )}
+            />
+            <Spacer />
+            <Text
+              h3
+              transform="full-width"
+              size={60}
+              css={{
+                textGradient: "45deg, $blue600 -20%, $pink600 50%",
+              }}
+              weight="bold"
+            >
+              Website
+            </Text>
+            <Spacer />
+            <Controller
+              name="website"
+              control={control}
+              fullWidth
+              render={({ field }) => (
+                <Input aria-label="website" fullWidth type="url" {...field} />
               )}
             />
           </>
