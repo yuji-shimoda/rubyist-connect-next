605ccc49262daed5ca7aac54de7958513776eee7
Yuji Shimoda <yuji.shimoda@gmail.com>
2022-09-10 14:54:44 +0900

    feature: add profile update function.

--- pages/settings/profile.js
+++ pages/settings/profile.js
@@ -1,24 +1,40 @@
-import { useEffect, useState } from 'react';
-import { withPageAuth } from '@supabase/auth-helpers-nextjs';
-import { useUser } from '@supabase/auth-helpers-react';
-import { Container, Grid, Textarea } from '@nextui-org/react';
-import { useRouter } from 'next/router';
-import { useForm, Controller } from 'react-hook-form';
-import AppBar from '../../components/AppBar';
+import { useEffect, useState } from "react";
+import { supabaseClient, withPageAuth } from "@supabase/auth-helpers-nextjs";
+import { useUser } from "@supabase/auth-helpers-react";
+import { Container, Grid, Spacer, Textarea, Button } from "@nextui-org/react";
+import { useRouter } from "next/router";
+import { useForm, Controller } from "react-hook-form";
+import AppBar from "../../components/AppBar";
 
-export const getServerSideProps = withPageAuth({ redirectTo: '/' });
+export const getServerSideProps = withPageAuth({ redirectTo: "/" });
 
 export default function UserPage() {
   const router = useRouter();
   const { user } = useUser();
   const [me, setMe] = useState({});
-  const { control, handleSubmit, setValue } = useForm({
+  const { control, handleSubmit, setValue, getValues } = useForm({
     defaultValues: {
-      firstName: '',
-      select: {},
+      introduction: "",
     },
   });
-  const onSubmit = (data) => console.log(data);
+
+  async function updateProfile() {
+    try {
+      const introduction = getValues("introduction");
+      const { data, error } = await supabaseClient
+        .from("profiles")
+        .update({ introduction: introduction, updated_at: new Date() })
+        .match({ id: user.id });
+      console.log(data);
+      if (error) {
+        throw error;
+      } else {
+        router.push(`/nnect/${user?.user_metadata?.user_name}`);
+      }
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
 
   useEffect(() => {
     async function loadData() {
@@ -27,7 +43,7 @@ export default function UserPage() {
         if (res.ok) {
           const data = await res.json();
           console.log(data);
-          setValue('introduction', data.pop().introduction);
+          setValue("introduction", data.pop().introduction);
         }
       } catch (error) {
         console.error(error);
@@ -40,29 +56,33 @@ export default function UserPage() {
     <>
       <AppBar user={user} />
       <Container>
-        <Grid.Container gap={2} justify="center">
-          <Grid sm md lg xl />
+        <Grid.Container gap={2} justify="center" css={{ mt: 50 }}>
           {me ? (
-            <form onSubmit={handleSubmit(onSubmit)}>
+            <form>
               <Controller
                 name="introduction"
                 control={control}
+                fullWidth
                 render={({ field }) => (
                   <Textarea
                     bordered
+                    rows={20}
                     width="800px"
+                    size="xl"
                     color="secondary"
                     labelPlaceholder="Introduction"
                     {...field}
                   />
                 )}
               />
-              <input type="submit" />
+              <Spacer />
+              <Button color="gradient" auto ghost onPress={updateProfile}>
+                登録
+              </Button>
             </form>
           ) : (
-            ''
+            ""
           )}
-          <Grid sm md lg xl />
         </Grid.Container>
       </Container>
     </>
