4d0b1e38b3191dd16691fae5c4aa379b56c2f27a
Yuji Shimoda <yuji.shimoda@gmail.com>
2022-09-10 16:21:42 +0900

    feature: modify style.

--- pages/settings/profile.js
+++ pages/settings/profile.js
@@ -1,7 +1,14 @@
 import { useEffect, useState } from "react";
 import { supabaseClient, withPageAuth } from "@supabase/auth-helpers-nextjs";
 import { useUser } from "@supabase/auth-helpers-react";
-import { Container, Grid, Spacer, Textarea, Button } from "@nextui-org/react";
+import {
+  Container,
+  Grid,
+  Text,
+  Textarea,
+  Button,
+  Spacer,
+} from "@nextui-org/react";
 import { useRouter } from "next/router";
 import { useForm, Controller } from "react-hook-form";
 import AppBar from "../../components/AppBar";
@@ -11,8 +18,7 @@ export const getServerSideProps = withPageAuth({ redirectTo: "/" });
 export default function UserPage() {
   const router = useRouter();
   const { user } = useUser();
-  const [me, setMe] = useState({});
-  const { control, handleSubmit, setValue, getValues } = useForm({
+  const { control, setValue, getValues } = useForm({
     defaultValues: {
       introduction: "",
     },
@@ -25,7 +31,6 @@ export default function UserPage() {
         .from("profiles")
         .update({ introduction: introduction, updated_at: new Date() })
         .match({ id: user.id });
-      console.log(data);
       if (error) {
         throw error;
       } else {
@@ -42,7 +47,6 @@ export default function UserPage() {
         const res = await fetch(`/api/users/${user?.user_metadata?.user_name}`);
         if (res.ok) {
           const data = await res.json();
-          console.log(data);
           setValue("introduction", data.pop().introduction);
         }
       } catch (error) {
@@ -56,33 +60,45 @@ export default function UserPage() {
     <>
       <AppBar user={user} />
       <Container>
-        <Grid.Container gap={2} justify="center" css={{ mt: 50 }}>
-          {me ? (
-            <form>
-              <Controller
-                name="introduction"
-                control={control}
-                fullWidth
-                render={({ field }) => (
-                  <Textarea
-                    bordered
-                    rows={20}
-                    width="800px"
-                    size="xl"
-                    color="secondary"
-                    labelPlaceholder="Introduction"
-                    {...field}
-                  />
-                )}
-              />
-              <Spacer />
-              <Button color="gradient" auto ghost onPress={updateProfile}>
-                登録
-              </Button>
-            </form>
-          ) : (
-            ""
-          )}
+        <Grid.Container gap={2} justify="center" css={{ mt: 20 }}>
+          <Grid sm md lg xl />
+          <>
+            <Text
+              h2
+              transform="full-width"
+              size={60}
+              css={{
+                textGradient: "45deg, $blue600 -20%, $pink600 50%",
+              }}
+              weight="bold"
+            >
+              Introduction
+            </Text>
+            <Spacer />
+            <Controller
+              name="introduction"
+              control={control}
+              fullWidth
+              render={({ field }) => (
+                <Textarea
+                  bordered
+                  minRows={15}
+                  rows={15}
+                  width="800px"
+                  size="xl"
+                  color="secondary"
+                  placeholder="Introduction"
+                  {...field}
+                />
+              )}
+            />
+          </>
+          <Grid sm md lg xl />
+        </Grid.Container>
+        <Grid.Container gap={2} justify="center" css={{ mt: 20 }}>
+          <Button color="gradient" auto ghost onPress={updateProfile}>
+            Update
+          </Button>
         </Grid.Container>
       </Container>
     </>
